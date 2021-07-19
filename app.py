ed0a1b4bd89083eacf7fc116700c6c3ed524fe83
Lijie Guo <lijieg@yoga.localdomain>
2021-07-18 21:13:29 -0400

    update app.py

--- app.py
+++ app.py
@@ -5,29 +5,33 @@ from compute import predict_user_controversial_items
 from compute import predict_user_hate_items
 from compute import predict_user_hip_items
 from compute import predict_user_no_clue_items
-
+from models import Rating
 
 app = Flask(__name__)
-
 
 @app.route('/preferences', methods=['POST'])
 def predict_preferences():
 
+    print("Testing")
     req = request.json
     user_id = req['user_id']
     ratings = req['ratings']
 
+    # convert ratings from dict to a data class, tested using Postman
+    ratings = [Rating(rating['item_id'], rating['rating']) for rating in ratings]
+
     preds = {
         'top_n': predict_user_topN,
-        'controversial': predict_user_controversial_items,
         'hate': predict_user_hate_items,
         'hip': predict_user_hip_items,
-        'no_clue': predict_user_no_clue_items
+        'no_clue': predict_user_no_clue_items,
+        'controversial': predict_user_controversial_items
     }
 
     for key, func in preds.items():
         preds[key] = func(ratings=ratings, user_id=user_id)
 
+    print(preds)
     return preds
 
 
